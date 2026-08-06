38e41e8a981e8091ca4a962950b78f8286d32891
mario <mario_reyes505@hotmail.com>
2026-08-05 22:45:58 -0500

    create Root.jsx , used as main navbar. Added it to App.jsx as root layout

--- src/App.jsx
+++ src/App.jsx
@@ -26,13 +26,20 @@ import EventsPage from "./pages/Events";
 import EventDetailPage from "./pages/EventDetail";
 import NewEventPage from "./pages/NewEvent";
 import EditEventPage from "./pages/EditEvent";
+import RootLayout from "./pages/Root";
 
 const router = createBrowserRouter([
-  { path: "/", element: <Homepage /> },
-  { path: "/events", element: <EventsPage /> },
-  { path: "/events/:eventId", element: <EventDetailPage /> },
-  { path: "/events/new", element: <NewEventPage /> },
-  { path: "/events/:eventId/edit", element: <EditEventPage /> },
+  {
+    path: "/",
+    element: <RootLayout />,
+    children: [
+      { index: true, element: <Homepage /> },
+      { path: "events", element: <EventsPage /> },
+      { path: "events/:eventId", element: <EventDetailPage /> },
+      { path: "events/new", element: <NewEventPage /> },
+      { path: "events/:eventId/edit", element: <EditEventPage /> },
+    ],
+  },
 ]);
 
 function App() {
